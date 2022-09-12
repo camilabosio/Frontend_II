dbd92c63ffb0aaac0f7c7c119fc44c6c68cc6ce5
camilabosio <cammibosio@gmail.com>
2022-09-12 17:08:25 -0300

    Trabajando con promesas

--- C16 - Introducción a asincronismo/enClase/script.js
+++ C16 - Introducción a asincronismo/enClase/script.js
@@ -1,5 +1,3 @@
-console.log('Clase 16 - AJAX');
-// La promesa puede cumpliser (resolve) o puede falla (reject)
 let miPromesa = new Promise( (resuelve, falla)=> {
 
     const cuenta = {
@@ -8,9 +6,9 @@ let miPromesa = new Promise( (resuelve, falla)=> {
         activo: true
     }
 
+    //Simulamos la demora de la promesa en el servidor
     setTimeout( function() {
         if( cuenta.saldo < 1  ){
-            // Saldo infucifiente
             falla({
                 mensaje: "Saldo insuficiente"
             }) 
@@ -24,20 +22,20 @@ let miPromesa = new Promise( (resuelve, falla)=> {
             }) 
         }
 
-    }, 5000 ) // Simulamos la demora en el servidor
+    }, 5000 ) 
 
 });
 
 const cajero = document.querySelector('.bancaMobile');
 const mensaje = document.querySelector('#mensaje');
 
-
+//Llamando a la promesa
 // Simulando demora en el servidor con carga asincrona
-miPromesa.then(  (respuesta)=> {  // resolve
+miPromesa.then(  (respuesta)=> {  // Camino en caso de resolve
     console.log('Respuesta Ok' + respuesta.mensaje)
     cajero.innerHTML= `<h4> Estado de la cuenta ${respuesta.mensaje} </h4>`;
 
-}).catch( (error)=> { // reject
+}).catch( (error)=> { // Camino en caso de reject
     console.log('error ' + error.mensaje)
     cajero.innerHTML= `<h4> Erro de la cuenta ${respuesta.mensaje} </h4>`;
 
